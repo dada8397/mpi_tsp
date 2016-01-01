93a57590b15469e2e244dcdcc17863951e7542fd
F74022028 <vincent8397@gmail.com>
2016-01-01 17:52:43 +0800

    First commit

---
+++ hw6.cpp
@@ -0,0 +1,12 @@
+#include <mpi.h>
+
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
+}
