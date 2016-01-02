a56c4c560d70ee82840ba3a6f09a33eccc52c136
F74022028 <vincent8397@gmail.com>
2016-01-02 22:58:49 +0800

    read file(not finished)

--- hw6.cpp
+++ hw6.cpp
@@ -1,17 +1,41 @@
-#include <mpi.h>
+//#include <mpi.h>
 
 #include <cstdio>
 #include <cstdlib>
+#include <string>
 #include <algorithm>
 #include <vector>
 using namespace std;
 
+int map[100][100];
+char* filename;
+int city_count;
+int population;
+int generations;
+double crossover;
+double mutation;
+int move;
+
 int main(int argc, char const *argv[]) {
-    /* code */
+
+    // 讀取參數檔
+    FILE *conf;
+    conf = fopen("config.conf", "r");
+    if(conf != NULL){
+        fscanf(conf, "%s", filename);     // 城市相鄰矩陣檔案名稱
+        printf("%s\n", filename);
+        fscanf(conf, "%d", &city_count);    // 城市數
+        printf("%d\n", city_count);
+        fscanf(conf, "%d", &population);    // 人口數
+        fscanf(conf, "%d", &generations);   // 世代數
+        fscanf(conf, "%f", &crossover);     // 交配機率
+        fscanf(conf, "%f", &mutation);      // 突變機率
+        fscanf(conf, "%d", &move);          // 人口遷移數
+        fclose(conf);
+    }else{
+        printf("參數檔不存在!\n");
+    }
+
+
     return 0;
-}
-
-void readFile(string filename){
-    FILE *fptr;
-
 }
