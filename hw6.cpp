80d19661129ec8160f2041dfa6330205595609df
F74022028 <vincent8397@gmail.com>
2016-01-05 15:21:52 +0800

    Not MPI yes

--- hw6.cpp
+++ hw6.cpp
@@ -1,4 +1,4 @@
-//#include <mpi.h>
+#include <mpi.h>
 
 #include <cstdio>
 #include <cstdlib>
@@ -15,6 +15,7 @@ vector<int> temp, cost, cost_child, child1, child2;
 vector<double> fitness, fitness_child;
 vector<double> posibility;
 int elite1, elite2;
+int my_rank, comm_sz;
 
 //Constants
 char filename[512];
@@ -26,16 +27,23 @@ double mutation_p;
 int immigration;
 
 //Functions
-void readFile(char*, int);
-void firstGeneration(int, int);
+void readFile();
+void firstGeneration();
 void findElite();
 bool sameCost(int);
 void calculatePosibility();
 int whellSelection();
+void crossover();
+void mutation();
+void immigrate();
 
 
 //Main function
 int main(int argc, char const *argv[]) {
+
+    MPI_Init(&argc,&argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
     //Read configure file
     FILE *conf;
@@ -45,8 +53,8 @@ int main(int argc, char const *argv[]) {
         fscanf(conf, "%d", &cities);        //Cites amount
         fscanf(conf, "%d", &population);    //Population amount
         fscanf(conf, "%d", &generations);   //Generation amount
-        fscanf(conf, "%lf", &crossover_p);    //Crossover probability
-        fscanf(conf, "%lf", &mutation_p);     //Mutation probability
+        fscanf(conf, "%lf", &crossover_p);  //Crossover probability
+        fscanf(conf, "%lf", &mutation_p);   //Mutation probability
         fscanf(conf, "%d", &immigration);   //Immigration amount
         fclose(conf);
     }else{
@@ -54,25 +62,47 @@ int main(int argc, char const *argv[]) {
     }
 
     //Initialize
-    readFile(filename, cities);
-    firstGeneration(cities, population);
+    readFile();
+    firstGeneration();
     findElite();
     calculatePosibility();
 
-    //Crossover
-    srand(time(NULL));
-    for(int i=0; i<population/2; i++){
-        child1.clear();
-        child2.clear();
-        crossover();
-        mutation();
+    srand(time(NULL) + my_rank);
+
+    //Crossover, Mutation
+    for(int j=0; j<generations; j++){
+        for(int i=0; i<(population - immigration)/2-1; i++){
+            child1.clear();
+            child2.clear();
+            crossover();
+            mutation();
+        }
+        gene_child.push_back(gene[elite1]);
+        gene_child.push_back(gene[elite2]);
+        cost_child.push_back(cost[elite1]);
+        cost_child.push_back(cost[elite2]);
+        fitness_child.push_back(fitness[elite1]);
+        fitness_child.push_back(fitness[elite2]);
+
+        gene = gene_child;
+        cost = cost_child;
+        fitness = fitness_child;
+
+        gene_child.clear();
+        cost_child.clear();
+        fitness_child.clear();
+        calculatePosibility();
+        findElite();
+
+        for(int i=0; i<immigration; i++){
+            immigrate();
+        }
     }
 
-    //for(int i=0; i<cost.size(); i++) printf("Cost: %d, fitness: %lf\n", cost[i], fitness[i]);
-    for(int i=0; i<generations; i++){
+    //Print elite
+    printf("%d\n", cost[elite1]);
 
-    }
-
+    MPI_Finalize();
     return 0;
 }
 
@@ -101,13 +131,10 @@ void firstGeneration() {
         for(int j=0; j<cities-1; j++)
             cost_sum += map[randomnum[j]][randomnum[j+1]];
         cost_sum += map[randomnum[0]][randomnum[cities-1]];
-        if(!sameCost(cost_sum)){    //If has same cost, we don't want this data.
-            i--;
-            continue;
-        }
         cost.push_back(cost_sum);
         fitness.push_back((double)1/(double)cost_sum);
         gene.push_back(temp);
+        temp.clear();
     }
 }
 
@@ -117,20 +144,16 @@ void findElite(){
     elite1 = 0, elite2 = 0;
     for(int i=0; i<cost.size(); i++){
         if(cost[i] < min){
-            elite2 = elite1;
             elite1 = i;
-            sec = min;
             min = cost[i];
         }
     }
-}
-
-//Cost the same
-bool sameCost(int _cost){
-    for(int i=0; i<cost.size(); i++)
-        if(_cost == cost[i])
-            return true;
-    return false;
+    for(int i=0; i<cost.size(); i++){
+        if(cost[i] < sec && cost[i] > min){
+            elite2 = i;
+            sec = cost[i];
+        }
+    }
 }
 
 //Calculate posibility
@@ -145,13 +168,13 @@ void calculatePosibility(){
 
 //Roulette Wheel Selection
 int whellSelection(){
-    srand(time(NULL));
-    double ran = rand() / RAND_MAX;
+    double ran = (double)rand() / RAND_MAX;
     int i = 0;
-    for(i=0; i<posibility.size(); i++){
-        if(ran < 0) return i;
-        else
+    while(ran > 0){
+        for(i=0; i<posibility.size(); i++){
             ran -= posibility[i];
+            if(ran < 0) return i;
+        }
     }
     return i;
 }
@@ -160,14 +183,12 @@ int whellSelection(){
 void crossover(){
     int dad = whellSelection();
     int mom = whellSelection();
-    while(dad == elite1 || dad == elite2)
-        dad = whellSelection();
-    while(mom == elite1 || mom == elite2)
+    while(dad == mom)
         mom = whellSelection();
 
-    double ran = rand() / RAND_MAX;
+    double ran = (double)rand() / RAND_MAX;
 
-    if(ran > crossover_p){
+    if(ran < crossover_p){
         int start = rand() % (cities - 1);
         int end = rand() % cities;
         while(start >= end)
@@ -177,12 +198,13 @@ void crossover(){
         dad_buf = gene[dad];
         mom_buf = gene[mom];
 
-        //First child, dad_buf, mom
         for(int i=start; i<end; i++){
             child1.push_back(gene[mom][i]);
+        }
+        for(int i=start; i<end; i++){
             for(int j=0; j<dad_buf.size(); j++){
-                if(dad_buf[j] == gene[mom][j]){
-                    dad_buf.erase(j);
+                if(dad_buf[j] == gene[mom][i]){
+                    dad_buf.erase(dad_buf.begin() + j);
                     break;
                 }
             }
@@ -191,12 +213,13 @@ void crossover(){
             child1.push_back(dad_buf[i]);
         }
 
-        //Second child, mom_buf, dad
         for(int i=start; i<end; i++){
             child2.push_back(gene[dad][i]);
+        }
+        for(int i=start; i<end; i++){
             for(int j=0; j<mom_buf.size(); j++){
-                if(mom_buf[j] == gene[dad][j]){
-                    mom_buf.erase(j);
+                if(mom_buf[j] == gene[dad][i]){
+                    mom_buf.erase(mom_buf.begin() + j);
                     break;
                 }
             }
@@ -208,14 +231,31 @@ void crossover(){
         child1 = gene[dad];
         child2 = gene[mom];
     }
+
+    //Calculate cost
+    int cost_sum = 0;
+
+    for(int j=0; j<cities-1; j++)
+        cost_sum += map[child1[j]][child1[j+1]];
+    cost_sum += map[child1[0]][child1[cities-1]];
+    cost_child.push_back(cost_sum);
+    fitness_child.push_back((double)1/(double)cost_sum);
+
+    cost_sum = 0;
+    for(int j=0; j<cities-1; j++)
+        cost_sum += map[child2[j]][child2[j+1]];
+    cost_sum += map[child2[0]][child2[cities-1]];
+    cost_child.push_back(cost_sum);
+    fitness_child.push_back((double)1/(double)cost_sum);
+
     gene_child.push_back(child1);
     gene_child.push_back(child2);
 }
 
 //Mutation
 void mutation(){
-    double ran = rand() / RAND_MAX;
-    if(ran >= mutation_p){
+    double ran = (double)rand() / RAND_MAX;
+    if(ran < mutation_p){
         int index1, index2, tmp;
         index1 = rand() % cities;
         index2 = rand() % cities;
@@ -226,4 +266,28 @@ void mutation(){
         child2[index1] = child2[index2];
         child2[index2] = tmp;
     }
+}
+
+//Immigrate
+void immigrate(){
+    int index = whellSelection();
+    vector<int> recv, send;
+    send = gene[index];
+    if(my_rank == 0){
+        MPI_Send(&send[0], cities, MPI_INT, my_rank + 1, 0, MPI_COMM_WORLD);
+        MPI_Recv(&recv[0], cities, MPI_INT, comm_sz - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }else if(my_rank == comm_sz - 1){
+        MPI_Send(&send[0], cities, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        MPI_Recv(&recv[0], cities, MPI_INT, my_rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }else{
+        MPI_Send(&send[0], cities, MPI_INT, my_rank + 1, 0, MPI_COMM_WORLD);
+        MPI_Recv(&recv[0], cities, MPI_INT, my_rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+    gene_child.push_back(recv);
+    cost_sum = 0;
+    for(int j=0; j<cities-1; j++)
+        cost_sum += map[recv[j]][recv[j+1]];
+    cost_sum += map[recv[0]][recv[cities-1]];
+    cost_child.push_back(cost_sum);
+    fitness_child.push_back((double)1/(double)cost_sum);
 }
